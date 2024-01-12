72333da264bf2d750dd1a961a4755783d5a55c58
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-01-12 14:06:58 +0600

    feat(menu screen): design_complete

--- lib/view/screens/menu/menu_screen.dart
+++ lib/view/screens/menu/menu_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:worker_bridge/utils/app_utils/app_icon/app_icons.dart';
 import 'package:worker_bridge/utils/app_utils/color/app_colors.dart';
 import 'package:worker_bridge/view/screens/menu/inner_widget/menu_account_section.dart';
 import 'package:worker_bridge/view/screens/menu/inner_widget/menu_help_section.dart';
