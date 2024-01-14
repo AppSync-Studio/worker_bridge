24f1fc9ad46fe081a9bbb5f9027c184180da2a73
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-01-15 02:33:04 +0600

    chore(sign up screen): use_custom_text_form_field_widget

--- lib/view/screens/sign_up/inner_widget/sign_up_form_section.dart
+++ lib/view/screens/sign_up/inner_widget/sign_up_form_section.dart
@@ -6,6 +6,7 @@ import 'package:worker_bridge/view/screens/sign_up/inner_widget/sign_up_bottom_s
 import 'package:worker_bridge/view/screens/sign_up/sign_up_controller.dart';
 import 'package:worker_bridge/view/widgets/bottom_sheet/custom_bottom_sheet.dart';
 import 'package:worker_bridge/view/widgets/buttons/custom_button_widget.dart';
+import 'package:worker_bridge/view/widgets/text_field/custom_text_form_field.dart';
 
 class SignUpFormSection extends StatelessWidget {
 
@@ -20,75 +21,21 @@ class SignUpFormSection extends StatelessWidget {
       child: Column(
         children: [
           // name
-          TextFormField(
-            cursorColor: AppColors.colorBlack,
-            controller: signUpController.nameController,
-            keyboardType: TextInputType.text,
+          CustomTextFormField(
+            textInputType: TextInputType.name,
             textInputAction: TextInputAction.next,
-            style: GoogleFonts.nunito(
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w400,
-                fontSize: 14
-            ),
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.transparent,
-                labelText: "Name",
-                labelStyle: GoogleFonts.nunito(
-                    color: AppColors.colorBlack,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                ),
-                prefixIcon: const Icon(Icons.person, size: 24, color: AppColors.colorGrey)
-            ),
+            textEditingController: signUpController.nameController,
+            labelText: "Name",
+            prefixIcon: const Icon(Icons.person, size: 24, color: AppColors.colorGrey)
           ),
           const Gap(12),
           // email
-          TextFormField(
-            cursorColor: AppColors.colorBlack,
-            controller: signUpController.emailController,
-            keyboardType: TextInputType.emailAddress,
+          CustomTextFormField(
+            textInputType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
-            style: GoogleFonts.nunito(
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w400,
-                fontSize: 14
-            ),
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.transparent,
-                labelText: "Email",
-                labelStyle: GoogleFonts.nunito(
-                    color: AppColors.colorBlack,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                ),
-                prefixIcon: const Icon(Icons.alternate_email, size: 24, color: AppColors.colorGrey)
-            ),
+            textEditingController: signUpController.emailController,
+            labelText: "Email",
+            prefixIcon: const Icon(Icons.alternate_email, size: 24, color: AppColors.colorGrey)
           ),
           const Gap(12),
           // phone number
@@ -96,265 +43,70 @@ class SignUpFormSection extends StatelessWidget {
             children: [
               Expanded(
                 flex: 1,
-                child: TextFormField(
+                child: CustomTextFormField(
                   readOnly: true,
-                  cursorColor: AppColors.colorBlack,
-                  controller: signUpController.codeController,
-                  keyboardType: TextInputType.phone,
-                  textInputAction: TextInputAction.next,
-                  style: GoogleFonts.nunito(
-                      color: AppColors.colorBlack,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14
-                  ),
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.transparent,
-                      labelText: "Code",
-                      labelStyle: GoogleFonts.nunito(
-                          color: AppColors.colorBlack,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                      )
-                  ),
+                  labelText: "Code",
+                  textEditingController: signUpController.codeController,
                   onTap: () => CustomBottomSheet(isDialCode: true, child: SignUpBottomSheetSection(signUpController: signUpController)).customBottomSheet(context),
-                ),
+                )
               ),
               const Gap(8),
               Expanded(
                 flex: 3,
-                child: TextFormField(
-                  cursorColor: AppColors.colorBlack,
-                  controller: signUpController.phoneNumberController,
-                  keyboardType: TextInputType.number,
+                child: CustomTextFormField(
+                  textInputType: TextInputType.number,
                   textInputAction: TextInputAction.next,
-                  style: GoogleFonts.nunito(
-                      color: AppColors.colorBlack,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14
-                  ),
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.transparent,
-                      labelText: "Phone Number",
-                      labelStyle: GoogleFonts.nunito(
-                          color: AppColors.colorBlack,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                      ),
-                      prefixIcon: const Icon(Icons.phone, size: 24, color: AppColors.colorGrey)
-                  ),
+                  textEditingController: signUpController.phoneNumberController,
+                  labelText: "Phone Number",
+                  prefixIcon: const Icon(Icons.phone, size: 24, color: AppColors.colorGrey)
                 ),
               )
             ],
           ),
           const Gap(12),
           // dob
-          TextFormField(
-            cursorColor: AppColors.colorBlack,
-            controller: signUpController.dobController,
-            textInputAction: TextInputAction.next,
-            style: GoogleFonts.nunito(
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w400,
-                fontSize: 14
-            ),
-            onTap: () => signUpController.pickDate(context),
+          CustomTextFormField(
             readOnly: true,
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.transparent,
-                labelText: "Date of Birth",
-                labelStyle: GoogleFonts.nunito(
-                    color: AppColors.colorBlack,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                ),
-                prefixIcon: const Icon(Icons.calendar_month, size: 24, color: AppColors.colorGrey)
-            ),
+            textEditingController: signUpController.dobController,
+            labelText: "Date of Birth",
+            prefixIcon: const Icon(Icons.calendar_month, size: 24, color: AppColors.colorGrey),
+            onTap: () => signUpController.pickDate(context),
           ),
           const Gap(12),
           // gender
-          TextFormField(
-            cursorColor: AppColors.colorBlack,
-            controller: signUpController.genderController,
-            textInputAction: TextInputAction.next,
-            style: GoogleFonts.nunito(
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w400,
-                fontSize: 14
-            ),
-            onTap: () => signUpController.pickGender(context),
+          CustomTextFormField(
             readOnly: true,
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.transparent,
-                labelText: "Gender",
-                labelStyle: GoogleFonts.nunito(
-                    color: AppColors.colorBlack,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                ),
-                prefixIcon: const Icon(Icons.male, size: 24, color: AppColors.colorGrey)
-            ),
+            textEditingController: signUpController.genderController,
+            labelText: "Gender",
+            prefixIcon: const Icon(Icons.male, size: 24, color: AppColors.colorGrey),
+            onTap: () => signUpController.pickGender(context),
           ),
           const Gap(12),
           // address
-          TextFormField(
-            cursorColor: AppColors.colorBlack,
-            controller: signUpController.addressController,
-            keyboardType: TextInputType.text,
+          CustomTextFormField(
+            textInputType: TextInputType.streetAddress,
             textInputAction: TextInputAction.next,
-            style: GoogleFonts.nunito(
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w400,
-                fontSize: 14
-            ),
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.transparent,
-                labelText: "Address",
-                labelStyle: GoogleFonts.nunito(
-                    color: AppColors.colorBlack,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                ),
-                prefixIcon: const Icon(Icons.place, size: 24, color: AppColors.colorGrey)
-            ),
+            textEditingController: signUpController.addressController,
+            labelText: "Address",
+            prefixIcon: const Icon(Icons.place, size: 24, color: AppColors.colorGrey)
           ),
           const Gap(12),
           // password
-          TextFormField(
-            obscureText: true,
-            cursorColor: AppColors.colorBlack,
-            controller: signUpController.passwordController,
-            keyboardType: TextInputType.visiblePassword,
+          CustomTextFormField(
+            isPassword: true,
+            textEditingController: signUpController.passwordController,
+            textInputType: TextInputType.visiblePassword,
             textInputAction: TextInputAction.next,
-            style: GoogleFonts.nunito(
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w400,
-                fontSize: 14
-            ),
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.transparent,
-                labelText: "Password",
-                labelStyle: GoogleFonts.nunito(
-                    color: AppColors.colorBlack,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                ),
-                suffixIcon: const Icon(Icons.visibility_off, size: 24, color: AppColors.colorGrey)
-            ),
+            labelText: "Password"
           ),
           const Gap(12),
           // confirm password
-          TextFormField(
-            obscureText: true,
-            cursorColor: AppColors.colorBlack,
-            controller: signUpController.confirmPasswordController,
-            keyboardType: TextInputType.visiblePassword,
+          CustomTextFormField(
+            isPassword: true,
+            textEditingController: signUpController.confirmPasswordController,
+            textInputType: TextInputType.visiblePassword,
             textInputAction: TextInputAction.done,
-            style: GoogleFonts.nunito(
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w400,
-                fontSize: 14
-            ),
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.transparent,
-                labelText: "Confirm Password",
-                labelStyle: GoogleFonts.nunito(
-                    color: AppColors.colorBlack,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.colorGrey, width: 1)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: AppColors.primaryColor, width: 1)
-                ),
-                suffixIcon: const Icon(Icons.visibility_off, size: 24, color: AppColors.colorGrey)
-            ),
+            labelText: "Confirm Password"
           ),
 
           const Gap(24),
